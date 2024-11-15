7a92ae7de2c7512192931f4583049303109e7127
pawaaan9 <pawankanchana99@gmail.com>
2024-11-15 23:47:43 +0530

    new

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,13 +19,13 @@ const About: React.FC = () => {
 
         {/* Content */}
         <p className="text-base md:text-lg text-[#D3D8F4] fade-in mb-6">
-          Hi, I’m Pawan Dhanapala from Sri Lanka! As a passionate Software Developer and
+          Hi, I&apos;m Pawan Dhanapala from Sri Lanka! As a passionate Software Developer and
           third-year IT undergraduate, I thrive on transforming ideas into
           impactful digital solutions. My journey in tech has been fueled by
           curiosity, continuous learning, and hands-on experience across web and
           mobile development. I'm excited to showcase my skills, projects, and
           vision for innovation. <br />
-          Let’s connect and build something great together!
+          Let&apos;s connect and build something great together!
         </p>
         
         <HoverEffect items={projects} className="mx-auto" />
